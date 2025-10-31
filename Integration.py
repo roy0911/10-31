4fc0b55efd0e9859e6efb1023b7da3ea066f733d
roy0911 <kevin2235417@gmail.com>
2025-10-31 11:20:28 +0800

    123

--- Integration.py
+++ Integration.py
@@ -69,9 +69,11 @@ def delete_student():
     cursor.execute('SELECT* from DB_student where db_student_id = ?'(student_id,))
     delete = cursor.fetchall()
     cursor.execute('DELETE from DB_student where db_student_id=?',(student_id))
-    print('Following row is delete:',delete)
+    print('Following row are delete:',delete)
     conn.commit()
 
+botton_delete = tk.Button(root, text='Delete' ,command=delete_student)
+botton_delete.pack(pady=25)
     
 
 root.mainloop() #must be put to the end of programming code
